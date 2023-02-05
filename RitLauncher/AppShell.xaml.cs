584d3e51f5d3533375f291ffaf0306ff80b6fe03
Kraskaska <k.krasilnikov.2008@gmail.com>
2023-02-05 11:11:32 +0100

    All downhill from here

---
+++ RitLauncher/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RitLauncher;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ RitLauncher/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace RitLauncher;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+        var versions = new List<string>();
+        versions.Add("Latest - branch master");
+        versions.Add("Latest - tag v0.0.1");
+        VersionPicker.ItemsSource = versions;
+        VersionPicker.SelectedItem = versions[0];
+    }
+}
